77e351da2a65f2a3c0361a8731a57a91604a7709
Samir Sardinha <samirssabreu@gmail.com>
2016-11-09 13:45:58 -0200

    Ajustes calculadora de desconto

--- DesignPatterns/CalculadorDeDescontos.cs
+++ DesignPatterns/CalculadorDeDescontos.cs
@@ -9,29 +9,29 @@ namespace DesignPatterns
     public class CalculadorDeDescontos
     {
 
-        public IList<Desconto> Descontos { get; private set; }
+        public IList<Desconto> ListaDescontos { get; private set; }
 
         public double Calcula(Orcamento orcamento)
         {
             //-- Adicionar os tipos de descontos existentes a lista para serem encadeados
-            this.Descontos.Add(new DescontoPorCincoItens() );
-            this.Descontos.Add(new DescontoPorMaisDeQuinhentosReais() );
+            this.ListaDescontos.Add(new DescontoPorCincoItens() );
+            this.ListaDescontos.Add(new DescontoPorMaisDeQuinhentosReais() );
 
-            this.Descontos.Add(new SemDesconto() ); //-- Importante! Deve ser o último!
+            this.ListaDescontos.Add(new SemDesconto() ); //-- Importante! Deve ser o último!
 
             //-- Definir a cadeia de execução dos descontos.
-            for ( int contador = 0; contador < Descontos.Count; contador++)
+            for ( int contador = 0; contador < ListaDescontos.Count; contador++)
             {
                 //-- Verificar se não é último desconto ( Sem desconto )
-                if ( contador + 1 < Descontos.Count) 
+                if ( contador + 1 < ListaDescontos.Count) 
                 {
                     //-- Definir que o próximo item da lista é o desconto a ser executado
-                    Descontos[contador].Proximo = Descontos[contador + 1];
+                    ListaDescontos[contador].Proximo = ListaDescontos[contador + 1];
                 }
             }
 
             //-- Executar o primeiro desconto e os demais serão executados em cadeia
-            return Descontos[0].Desconta(orcamento);
+            return ListaDescontos[0].Desconta(orcamento);
         }
     }
 }
